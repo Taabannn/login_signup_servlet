967f07663a65e6327426d6ce9c20ee342eaceede
unknown <tabansoleymani@yahoo.com>
2021-12-29 17:26:59 -0800

    add UserService & UserAndPassValidator

---
+++ src/main/java/ir/maktab58/service/UserService.java
@@ -0,0 +1,24 @@
+package ir.maktab58.service;
+
+import ir.maktab58.dao.UserDao;
+import ir.maktab58.entity.User;
+
+/**
+ * @author Taban Soleymani
+ */
+public class UserService {
+    private final UserDao userDao = new UserDao();
+
+    public int saveNewUser(String username, String password) {
+        UserAndPassValidator instance = UserAndPassValidator.getInstance();
+        boolean userAndPassValid = instance.isUserAndPassValid(username, password);
+
+        User user = new User();
+        user.setUsername(username);
+        user.setPassword(password);
+
+        if (userAndPassValid)
+            return userDao.save(user);
+        return 0;
+    }
+}
